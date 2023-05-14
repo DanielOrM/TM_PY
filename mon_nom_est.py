1611f94ee44da4596081d1adb06facdc3c88872c
DanielOrM <orihuelasdaniel@gmail.com>
2023-05-14 15:15:44 +0200

    ajout: - classe Control(ttk.frame) - si touches a ou d appuyées ==> change couleur fenêtre
    A CHANGER:
    - couleur ==> nouvelle fenêtre = 1pièce
    - implémentation d'un switch entre frame

--- mon_nom_est.py
+++ mon_nom_est.py
@@ -2,6 +2,17 @@ import tkinter as tk
 from tkinter import ttk
 
 
+# class RoomModel(ttk.Frame):
+#     def __init__(self, png):  # couleur pour tester puis ce sera changé en png pour chaque pièce
+#         super().__init__(png)
+#         self.png = png
+#         self.configure(background="{0}".format(png))
+#
+#
+# Room1 = RoomModel("blue")
+# Room2 = RoomModel("green")
+
+
 class App(tk.Tk):
     """
     Application:
@@ -12,27 +23,45 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Mon nom est...")
+        self.index = 2
+        self.bg = None
+        # toutes les pièces (couleurs) dans une liste, pièce initiale = 0
+        # chaque couleur = 1 pièce, TEMPORAIRE (après png)
+        # self.COLORS = [(""), #
+        #                (), # -2 -1, -2=DERNIÈRE PIÈCE
+        #                (), # -2 -1 0
+        #                (), # -1, 0, 1
+        #                (), # 0 1 2
+        #                (), # 1 2, 2=DERNIÈRE PIÈCE
+        #     ]
+        self.COLORS = ["red", "blue", "green", "yellow", "pink"]
+        # self.ROOMS = [Room1, Room2, "blue"]
+        self.configure(background="{0}".format(self.COLORS[self.index]))
+        # self.configure(background="{0}".format(self.ROOMS[self.index]))
+        # self.configure()
         # dimensions écran
         self.screen_width = self.winfo_screenwidth()
         self.screen_height = self.winfo_screenheight()
         self.width = int(self.screen_width/2)
         self.height = int(self.screen_height/2)
-        # position x et y du centre écran
+        # position x et y centre écran
         self.center_x = int((self.screen_width/4))
         self.center_y = int((self.screen_height/4))
-        # applique dimensions initiales à fenêtre + centre fenêtre
+        # application dimensions initiales à fenêtre + centre fenêtre
         self.geometry("{0}x{1}+{2}+{3}".format(
             self.width, self.height, self.center_x, self.center_y
             )
         )
         # widgets
         self.w = FullScreenWindow(self)
+        self.c = Control(self)
+        # self.r = RoomModel(self)
 
 
 class FullScreenWindow(ttk.Frame):
     """
     Événements gérés:
-    - plein écran <F11>
+    - plein écran <F11> ou <Fn> + <F11>
     - <Escape> pour quitter plein écran
     """
     def __init__(self, master):
@@ -42,7 +71,7 @@ class FullScreenWindow(ttk.Frame):
         master.bind("<F11>" or "<Fn> + <F11>", self.toggle_fullscreen)
         master.bind("<Escape>", self.exit_fullscreen)
 
-    def toggle_fullscreen(self, event):
+    def toggle_fullscreen(self, event=None):
         if self.is_same_action == 0:
             self.is_fullscreen = not self.is_fullscreen  # toggle boolean
             self.master.attributes("-fullscreen", self.is_fullscreen)
@@ -78,6 +107,42 @@ class FullScreenWindow(ttk.Frame):
 #
 #     def __init__(self):
 #         pass
+
+
+class Control(ttk.Frame):
+    """
+    Événements gérés:
+    -
+    """
+    def __init__(self, master):
+        super().__init__(master)
+        master.bind("<a>", self.change_room_left)
+        master.bind("<d>", self.change_room_right)
+
+    def change_room_left(self, event=None):
+        if self.master.index > 0:
+            # room_chosen = self.master.COLORS[index_room]
+            self.master.index -= 1
+            index_room = self.master.index
+            self.master.configure(background="{0}".format(self.master.COLORS[index_room]))
+            # self.master.configure(background="{0}".format(self.master.ROOMS[index_room]))
+            print("TEST")
+        else:
+            print("C'est un mur...")
+        return "break"
+
+    def change_room_right(self, event=None):
+        if self.master.index < 4:
+            # room_chosen = self.master.COLORS[index_room]
+            self.master.index += 1
+            index_room = self.master.index
+            self.master.configure(background="{0}".format(self.master.COLORS[index_room]))
+            # self.master.configure(background="{0}".format(self.master.ROOMS[index_room]))
+            print("TEST")
+        else:
+            print("C'est un mur...")
+        return "break"
+
 
 if __name__ == "__main__":
     # w = FullScreenWindow(root)
